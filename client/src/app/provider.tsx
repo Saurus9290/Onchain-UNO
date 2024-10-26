6be0fe55acc479f5ae1bfa9c43244b758bb3a3ba
saurabh <sauraj9290@gmail.com>
2024-10-26 14:46:44 +0530

    added landing page

---
+++ client/src/app/provider.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import '@rainbow-me/rainbowkit/styles.css';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { config } from "../lib/wagmi";
+import { PrivyProvider } from '@privy-io/react-auth';
+import { WagmiProvider } from '@privy-io/wagmi';
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }) {
+  return (
+    <PrivyProvider
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID as string}
+      config={{
+        // Customize Privy's appearance in your app
+        appearance: {
+          theme: 'light',
+          accentColor: '#ed5f1c',
+          logo: '',
+        },
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          createOnLogin: 'users-without-wallets',
+        },
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <WagmiProvider config={config}>
+          {children}
+        </WagmiProvider>
+      </QueryClientProvider>
+    </PrivyProvider>
+  );
+}
---
+++ client/src/components/homePage/Footer.jsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { socials } from '../../constants';
+
+import styles from '../styles';
+import { footerVariants } from '@/util/motion';
+
+const Footer = () => (
+  <motion.footer
+    variants={footerVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+    data-testid="footer"
+  >
+    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
+      <div className="flex items-center justify-center flex-wrap gap-5">
+        <h4 className="font-bold md:text-[64px] text-[44px] text-white">
+          Enter the GamePlay
+        </h4>
+      </div>
+
+      <div className="flex flex-col">
+        <div className="mb-[50px] h-[2px] bg-white opacity-10" />
+        <div className="flex items-center justify-between flex-wrap gap-4">
+          <h4 className="font-extrabold text-[24px] text-white">
+            UNO
+          </h4>
+          <p className="font-normal text-[14px] text-white opacity-50">
+            Copyright © 2024 - 2025 UNO. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </div>
+  </motion.footer>
+);
+
+export default Footer;
